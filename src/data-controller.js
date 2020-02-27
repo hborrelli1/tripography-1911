c8741366bad10c369af6c27ab5fd918f3e667452
Harry Borrelli <hborrelli@outlook.com>
2020-02-27 15:39:55 -0700

    Direct user depending on login.

--- src/data-controller.js
+++ src/data-controller.js
@@ -1,17 +1,18 @@
 class DataController {
   constructor() {
-    
+
   }
 
   getTravelers() {
-    let allTravelers;
-
-    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+    // let allTravelers;
+    console.log('getTravelers method firing:');
+    
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
       .then(res => res.json())
-      .then(data => allTravelers = data)
+      .then(data => data)
       .catch(err => console.log(err.message));
 
-    return allTravelers;
+    // return allTravelers;
   }
 }
 
