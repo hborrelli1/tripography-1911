4b9e412263fcfd515bb29e9117b472de5bacc976
Harry Borrelli <hborrelli@outlook.com>
2020-02-29 20:50:56 -0700

    Create dashboard for agent minus travelers for today section.

--- src/traveler.js
+++ src/traveler.js
@@ -1,12 +1,14 @@
+import User from './user';
 import Trip from './trip';
 
-class Traveler {
-  constructor(travelerInfo, myTrips) {
+class Traveler extends User {
+  constructor(userType, myTrips, travelerInfo) {
+    super(userType, myTrips);
     this.id = travelerInfo.id;
     this.name = travelerInfo.name;
     this.travelerType = travelerInfo.travelerType;
     this.myTrips = myTrips;
-    this.totalSpent = this.getTotalAmountSpentThisYear();
+    this.totalSpent = this.getTotalAmountSpentThisYear(myTrips, userType);
   }
 
   makeTripRequest(tripInfo) {
@@ -18,26 +20,26 @@ class Traveler {
   // Calculate flight cost * travelers
   // Calculate Cost per day * duration
   // Add prev two together, and get 10% charge
-  getTotalAmountSpentThisYear() {
-    let currentYear = new Date().getFullYear();
-
-    let thisYearsTrips = this.myTrips.filter(trip => {
-      let tripYear = Number(trip.date.substring(0, 4));
-      return tripYear === currentYear;
-    });
-
-    let yearlyCost = thisYearsTrips.reduce((totalSpent, trip) => {
-      let flightCost = trip.destinationInfo.estimatedFlightCostPerPerson * trip.travelers;
-      let lodgingCost = trip.destinationInfo.estimatedLodgingCostPerDay * trip.duration;
-      let tripTotal = flightCost + lodgingCost;
-
-      return totalSpent += tripTotal;
-    }, 0)
-
-    let yearlyTotal = yearlyCost + (yearlyCost * .10);
-
-    return yearlyTotal.toLocaleString("en-US", { style: "currency", currency: "USD" });
-  }
+  // getTotalAmountSpentThisYear() {
+  //   let currentYear = new Date().getFullYear();
+  //
+  //   let thisYearsTrips = this.myTrips.filter(trip => {
+  //     let tripYear = Number(trip.date.substring(0, 4));
+  //     return tripYear === currentYear;
+  //   });
+  //
+  //   let yearlyCost = thisYearsTrips.reduce((totalSpent, trip) => {
+  //     let flightCost = trip.destinationInfo.estimatedFlightCostPerPerson * trip.travelers;
+  //     let lodgingCost = trip.destinationInfo.estimatedLodgingCostPerDay * trip.duration;
+  //     let tripTotal = flightCost + lodgingCost;
+  //
+  //     return totalSpent += tripTotal;
+  //   }, 0)
+  //
+  //   let yearlyTotal = yearlyCost + (yearlyCost * .10);
+  //
+  //   return yearlyTotal.toLocaleString("en-US", { style: "currency", currency: "USD" });
+  // }
 }
 
 export default Traveler;
