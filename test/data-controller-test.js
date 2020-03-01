dbc04cd6bce167ac15d4a4c21ed6846dcb8ab063
Harry Borrelli <hborrelli@outlook.com>
2020-03-01 15:05:00 -0700

    Build tests

--- test/data-controller-test.js
+++ test/data-controller-test.js
@@ -3,6 +3,9 @@ const expect = chai.expect;
 
 import DataController from '../src/data-controller';
 
+const spies = require('chai-spies');
+chai.use(spies);
+
 describe('DataController Tests', function() {
   let dataController;
 
@@ -10,8 +13,14 @@ describe('DataController Tests', function() {
     dataController = new DataController();
   });
 
-  // it('should be able to instantiate & inherit properties', function() {
-  //   expect(dataController.getUsersTrips(40).length).to.equal(6);
-  // });
+  it('should be able to instantiate & inherit properties', function() {
+    chai.spy.on(
+      fetch,
+      [method(s) to listen for],
+      [replacement behavior]
+    )
+    expect(dataController.getUsersTrips(40).length).to.equal(6);
+  });
+
 
 });
